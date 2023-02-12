628cb3b40a452bfe3f14a2b018a415068653906a
Mohaiminul Islam Shishir <shishir@Mohaiminuls-Mac-mini.local>
2023-02-12 15:59:07 +0600

    user login and error handling done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { setUser } from "./features/auth/authSlice";
 import auth from "./firebase/firebase.config";
 import routes from "./routes/routes";
 
 function App() {
+  const { isLoading } = useSelector((state) => state.auth);
+  console.log(isLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -14,10 +17,11 @@ function App() {
         dispatch(setUser(user.email));
       }
     });
-  }, []);
+  }, [dispatch]);
   return (
     <>
       <RouterProvider router={routes} />
+      <Toaster />
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 
 import { useNavigate } from "react-router-dom";
 import loginImage from "../assets/login.svg";
 import { googleLogin, loginUser } from "../features/auth/authSlice";
 const Login = () => {
-  const { isLoading, email } = useSelector((state) => state.auth);
+  const { isLoading, email, isError, error } = useSelector(
+    (state) => state.auth
+  );
   const { register, handleSubmit, reset } = useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -21,7 +24,11 @@ const Login = () => {
 
   useEffect(() => {
     if (!isLoading && email) navigate("/");
-  }, [isLoading]);
+  }, [email, isLoading, navigate]);
+
+  useEffect(() => {
+    if (isError) toast.error(error);
+  }, [isError, error]);
 
   return (
     <div className="flex h-screen items-center">
